c76509f483ce9939bbff40192e18e422f8c9c332
Carlos Suarez <kazh027@gmail.com>
2016-05-10 13:56:59 -0500

    Update Player.java

--- core/src/mx/heroesofanzu/game/sprites/Player.java
+++ core/src/mx/heroesofanzu/game/sprites/Player.java
@@ -1,29 +1,25 @@
 package mx.heroesofanzu.game.sprites;
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-
 import mx.heroesofanzu.game.screens.PlayScreen;
 import mx.heroesofanzu.game.util.DirectionGestureDetector;
 
 /**
  * Created by jesusmartinez on 12/04/16.
+ * Modificated by carlossuarez 9/5/16
  */
 public class Player extends Entity {
 
-	private Vector2 direction;
+    private Vector2 direction;
 
 	public Player(PlayScreen screen, float x, float y) {
 		super(screen, x, y);
 		direction = new Vector2(16f, 0);
 	}
 
-	@Override
-	protected void defineEnemy() {
-		super.defineEnemy();
-		setPlayerGestures();
-	}
 
 	@Override
 	public void update(float delta) {
@@ -70,5 +66,17 @@ public class Player extends Entity {
 				direction.set(0, -50);
 			}
 		}));
+	}
+
+	@Override
+	protected void defineBody() {
+		TextureRegion[] frames = new TextureRegion[6];
+		for(int i=0; i < 6; i++) {
+			setSpriteSheet(new Texture("GoldCoinSprite/coin" + (i + 1) + ".png"));
+			setTextureRegion(new TextureRegion(getSpriteSheet(), 30, 30));
+			frames[i] = getTextureRegion();
+			setBodyAnimation(new Animation(0.1f, frames));
+		}
+        setPlayerGestures();
 	}
 }
